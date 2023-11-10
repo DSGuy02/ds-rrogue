fd824458b1727109f0a1bbc6983a17ad1fa5554b
Oladeji Sanyaolu <oladejisanyaolu@gmail.com>
2023-11-10 17:35:21 +0000

    initial commit

---
+++ components.go
@@ -0,0 +1,14 @@
+// Code written by Oladeji Sanyaolu (components.go) 09/11/2023
+
+package main
+
+type Player struct{}
+
+type Position struct {
+	X int
+	Y int
+}
+
+type Renderable struct{}
+
+type Movable struct{}
---
+++ dungeon.go
@@ -0,0 +1,8 @@
+// Code written by Oladeji Sanyaolu (dungeon.go) 08/11/2023
+package main
+
+// Dungeon is a container for all the levels that make up a particular dungeon in the world.
+type Dungeon struct {
+	Name   string
+	Levels []Level
+}
